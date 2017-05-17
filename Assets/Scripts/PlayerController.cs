a3ef1d918a11fa32469b57ff311fb940af1988d1
Patrick Brisbine <patrickbrisbine@gmail.com>
2017-05-16 20:31:17 -0700

    Initial push of project

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    /// <summary>
+    /// Called before every frame is rendered
+    /// </summary>
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// Called before performing physics calculations
+    /// </summary>
+    void FixedUpdate()
+    {
+        Input 
+    }
+}
