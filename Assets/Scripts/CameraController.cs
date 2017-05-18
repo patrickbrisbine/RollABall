6bcf16f45f6a3e0f411b1e02edf79e2a2a3c057b
Patrick Brisbine <patrickbrisbine@gmail.com>
2017-05-17 20:46:29 -0700

    Adding walls and physics (completed lesson 4)

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+	}
+	
+	// LateUpdate is called once per frame, but is called after all calculations are made
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;		
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,6 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
+	private Rigidbody rb;
+	public float speed = 1;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
 
     /// <summary>
     /// Called before every frame is rendered
@@ -17,6 +24,11 @@ public class PlayerController : MonoBehaviour {
     /// </summary>
     void FixedUpdate()
     {
-        Input 
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
     }
 }
