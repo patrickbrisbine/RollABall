00ca5013428d71f21dc9ad911f5a226d23b490fe
Patrick Brisbine <patrickbrisbine@gmail.com>
2017-05-18 21:27:13 -0700

    Completed Roll A Ball tutorial

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,29 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 	private Rigidbody rb;
 	public float speed = 1;
+	public Text scoreText;
+	public Text winText;
 
-	void Start()
-	{
+	public string time;
+
+	private int score;
+
+	void Start() {
 		rb = GetComponent<Rigidbody>();
+		score = 0;
+		setScoreText ();
+		winText.text = "";
 	}
 
     /// <summary>
     /// Called before every frame is rendered
     /// </summary>
-    void Update()
-    {
-        
+    void Update() {
+		if (score < 13) {
+			time = Time.realtimeSinceStartup.ToString ();
+		}
+		setScoreText ();
     }
 
     /// <summary>
     /// Called before performing physics calculations
     /// </summary>
-    void FixedUpdate()
-    {
+    void FixedUpdate() {
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
 
@@ -31,4 +41,21 @@ public class PlayerController : MonoBehaviour {
 
 		rb.AddForce (movement * speed);
     }
+
+	void setScoreText() {
+		scoreText.text = "Score: " + score + "\r\nDuration: " + time;
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.CompareTag ("Pickup")) {
+			other.gameObject.SetActive (false);
+			score = score + 1;
+			if (score == 13) {
+				time = Time.realtimeSinceStartup.ToString ();
+				winText.text = "You Win!";
+			}
+
+			setScoreText ();
+		}
+	}
 }
